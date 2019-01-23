eae6e287743b99ea504178f6f43da7c7b5273712
student <student@Veena-2.local>
2019-01-23 16:08:18 +0530

    Committing initial screens

---
+++ tryGit/tryGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tryGit
+//
+//  Created by student on 23/01/19.
+//  Copyright © 2019 studentCreative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
